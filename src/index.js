895d0d05b11acbc5b071f96c1ab42f32a00f07ea
Jefferson William <jeffersonwilliammachado@gmail.com>
2019-12-20 13:11:21 -0300

    Padronizado códigos anteriores via Prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 const isLocalhost = window.location.host.indexOf('localhost:3000') > -1
 const serviceWorkerCondition = isLocalhost ? 'unregister' : 'register'
 
-serviceWorker[serviceWorkerCondition]();
+serviceWorker[serviceWorkerCondition]()
