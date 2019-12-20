90bab35c0e4741e4ed5deee3f1b603b462203a76
Jefferson William <jeffersonwilliammachado@gmail.com>
2019-12-20 12:57:48 -0300

    Limpando projeto

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const isLocalhost = window.location.host.indexOf('localhost:3000') > -1
+const serviceWorkerCondition = isLocalhost ? 'unregister' : 'register'
+
+serviceWorker[serviceWorkerCondition]();
