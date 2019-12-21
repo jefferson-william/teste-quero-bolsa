964dab8c97a789cdb3672647ab19a61ea735fa63
Jefferson William <jeffersonwilliammachado@gmail.com>
2019-12-21 19:03:42 -0300

    Ativando hot loader

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import { hot } from 'react-hot-loader/root'
 import GlobalStyles from '~/styles/global'
 import GlobalFonts from '~/styles/fonts'
 import Header from '~/components/Header'
 import Navigation from '~/components/Navigation'
+import Breadcrumb from '~/components/Breadcrumb'
 import { App } from './styles'
 
-export default () => {
+function Initial() {
   return (
     <App>
       <GlobalStyles />
       <GlobalFonts />
       <Header />
       <Navigation />
+      <Breadcrumb />
     </App>
   )
 }
+
+export default hot(Initial)
